b87340df22eaeaf5c03eb0c9c0b91f70a1a84e77
robertholf <rob@bertholf.com>
2015-01-06 18:03:06 -0800

    Update

--- wp-plugin-master.php
+++ wp-plugin-master.php
@@ -5,7 +5,6 @@ Plugin URI: http://rbplugin.com
 Description: Example plugin.
 Author: bertholf
 Author URI: http://rob.bertholf.com
-Text Domain: rbplugin
 Version: 3.0
 */
 $RBPlugin_VERSION = "3.0";
@@ -43,7 +42,10 @@ License: None
 	// Paths
 	define( 'RBPlugin_PLUGIN_PATH', plugin_dir_url(__FILE__) );
 	define( 'RBPlugin_PLUGIN_DIR', plugin_dir_path(__FILE__) );
-	define( 'RBPlugin_TEXTDOMAIN', 'rbplugin' );
+	define( 'RBPlugin_SLUG', plugin_basename(__FILE__) );
+	define( 'RBPlugin_TEXTDOMAIN', RBPlugin_SLUG );
+	// Server
+	define( 'RBPlugin_LICENSE_PATH', 'http://rbplugin.com/license/' );
 
 
 // *************************************************************************************************** //
@@ -56,8 +58,11 @@ License: None
 	require_once( RBPlugin_PLUGIN_DIR .'lib/RBPlugin-Init.php'); // WP Related
 		add_action( 'init', array('RBPlugin_Init', 'init') ); // Menu/Internationalization etc.
 
-	require_once( RBPlugin_PLUGIN_DIR .'lib/RBPlugin-Update.php'); // Admin Specific
-		// Check if new version available
+	require_once( RBPlugin_PLUGIN_DIR .'lib/RBPlugin-Update.php'); // Update Specific
+		add_action( 'init', 'update_check' );
+			function update_check() {
+				new wp_auto_update (RBPlugin_VERSION, RBPlugin_LICENSE_PATH, RBPlugin_SLUG);
+			}
 
 	require_once( RBPlugin_PLUGIN_DIR .'lib/RBPlugin-Common.php'); // Common Functions
 
